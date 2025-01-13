8bafba16ed52fd447da8c9ff17ac244d8089cb3b
StefanMogren <stefan_mogren@hotmail.com>
2025-01-13 11:59:33 +0100

    Finished tasks 1 to 17.

---
+++ script.js
@@ -0,0 +1,120 @@
+// 1.
+/*
+const fruitsArray = ["apple", "banana", "orange", "kiwi", "grape"]
+
+console.log(fruitsArray)
+*/
+
+
+// 2.
+/*
+
+const mixedArray = [12, "thirteen", ["alpha", 5, "omega"]];
+
+console.log(mixedArray)
+*/
+
+
+// 3. - 6.
+/*
+let animals = ["cat", "hamster", "parrot", "funky chihuahua"];
+
+console.log(animals.length);
+
+// 4.
+console.log(animals[0]);
+
+// 5.
+console.log(animals[3]);
+
+// 6.
+
+animals[1] = "tiger";
+
+console.log(animals);
+*/
+
+
+// 7.
+/*
+let a = [1, 2, 3];
+let b = [4, 5, 6];
+
+a.push(...b);
+console.log(a);
+
+//let c = a.concat(b);
+//console.log(c);
+*/
+
+
+// 8. 
+/*
+let a = [1, 2, 3, 7, 8, 9];
+let b = [4, 5, 6];
+
+a.splice(3, 0, ...b);
+console.log(a)
+*/
+
+
+// 9.
+/*
+let arr = ["a", "b", "c"];
+let arr2 = Array.from(arr);
+
+console.log(arr2);
+*/
+
+
+// 10. - 15
+let fruits = ["kiwi", "apple", "pear"];
+
+//fruits.push("banana");
+
+//console.log(fruits);
+
+
+// 11.
+/*
+// fruits.unshift("orange");
+
+// console.log(fruits);
+
+
+// 12.
+// fruits.pop();
+// console.log(fruits);
+
+
+// 13.
+// fruits.shift();
+// console.log(fruits);
+
+
+// 14.
+// fruits.splice(1, 0, "grapes");
+// console.log(fruits);
+
+
+// 15.
+fruits.splice(2, 0, "grapes", "orange", "pineapple");
+console.log(fruits);
+*/
+
+
+// 16.
+/*
+let names = ["David", "Christoffer", "Johan", "Maja"];
+names.splice(1, 2);
+console.log(names);
+*/
+
+
+// 17.
+/*
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+nums = nums.reverse();
+
+console.log(nums);
+*/
